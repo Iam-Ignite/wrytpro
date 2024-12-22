c55ebb7bb059f762708e2aa2b366f49cff4ca54d
uru <ur>
2024-12-23 00:08:00 +0100

    wryta proh

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
       <header className="bg-orange-500 text-white px-6 py-4 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
         <div className="flex flex-col md:flex-row items-center md:space-x-3">
           <h1 className="text-xl font-bold uppercase">My Assignment Help™</h1>
-          <span className="text-sm">World's No.1 Essay & Assignment Help Co.</span>
+          <span className="text-sm">World&apos;s No.1 Essay & Assignment Help Co.</span>
         </div>
         <button className="bg-green-600 px-4 py-2 rounded-md text-sm md:text-base">
           New Assignment
@@ -100,7 +100,7 @@ const Dashboard = () => {
             <h3 className="font-semibold mb-2 text-gray-800">Refer & Earn</h3>
             <p className="text-sm text-gray-500">
               Get <span className="font-bold">FLAT 20% CASH</span> for every
-              friend's order for life!
+              friend&apos;s order for life!
             </p>
             <button className="bg-blue-500 text-white px-3 py-1 mt-2 rounded-md">
               Earn Now
